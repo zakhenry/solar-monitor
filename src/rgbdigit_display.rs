e45a8810f892ee6c86abdf92590be7d2556c18f0
Zak Henry <zak@cloudnc.com>
2024-01-04 04:48:35 +1300

    Colour tweaks, noise around zero tweaks

--- src/rgbdigit_display.rs
+++ src/rgbdigit_display.rs
@@ -32,7 +32,7 @@ impl SolarStatusDisplay for RgbDigitDisplay<'_> {
 
 
     fn show_status(&mut self, status: SolarStatus) -> Result<(), SolarMonitorError> {
-        let solar_generation_kw: f32 = status.solar_power_watts as f32 / 1000.0;
+        let solar_generation_kw: f32 = status.solar_power_watts.clamp(0, i32::MAX) as f32 / 1000.0;
         let solar_generation_formatted = format!("{solar_generation_kw:.1}");
 
         &self.solar_generation_status.set_value(solar_generation_formatted);
@@ -42,26 +42,26 @@ impl SolarStatusDisplay for RgbDigitDisplay<'_> {
         let house_consumption_kw: f32 = status.house_power_watts as f32 / 1000.0;
         let house_consumption_formatted = format!("{house_consumption_kw:.1}");
         &self.house_consumption_status.set_value(house_consumption_formatted);
-        &self.house_consumption_status.set_color((0, 0, 100));
+        &self.house_consumption_status.set_color((30, 10, 80));
         &self.house_consumption_status.write();
 
         let battery_kw: f32 = (status.battery_power_watts as f32 / 1000.0).abs();
         let battery_formatted = format!("{battery_kw:.1}");
         &self.battery_status.set_value(battery_formatted);
-        if status.battery_power_watts < 0 {
-            &self.battery_status.set_color((0, 100, 0));
-        } else {
+        if status.battery_power_watts > -100 { // epsilon to stop it from flickering while around zero
             &self.battery_status.set_color((100, 40, 10));
+        } else {
+            &self.battery_status.set_color((30, 70, 20));
         }
         &self.battery_status.write();
 
         let grid_kw: f32 = (status.grid_power_watts as f32 / 1000.0).abs();
         let grid_formatted = format!("{grid_kw:.1}");
         &self.grid_status.set_value(grid_formatted);
-        if status.grid_power_watts < 0 {
-            &self.grid_status.set_color((30, 30, 30));
-        } else {
+        if status.grid_power_watts > 100 { // epsilon to stop it from flickering while around zero
             &self.grid_status.set_color((50, 0, 0));
+        } else {
+            &self.grid_status.set_color((30, 30, 30));
         }
         &self.grid_status.write();
 
